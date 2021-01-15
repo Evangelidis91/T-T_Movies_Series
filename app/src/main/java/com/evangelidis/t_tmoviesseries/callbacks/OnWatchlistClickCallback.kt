b47715dc87081b2bce764cc7f28cb7b1d35e98b5
Evangelidis <kevangelidis@nn4m.com>
2021-01-15 21:55:41 +0000

    CleanUp WatchlistActivity

--- app/src/main/java/com/evangelidis/t_tmoviesseries/callbacks/OnWatchlistClickCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.evangelidis.t_tmoviesseries.callbacks
-
-import com.evangelidis.t_tmoviesseries.room.WatchlistData
-
-interface OnWatchlistClickCallback {
-    fun onClick(watchlist: WatchlistData)
-}
--- app/src/main/java/com/evangelidis/t_tmoviesseries/view/watchlist/WatchlistAdapter.kt
+++ app/src/main/java/com/evangelidis/t_tmoviesseries/view/watchlist/WatchlistAdapter.kt
@@ -1,69 +1,84 @@
 package com.evangelidis.t_tmoviesseries.view.watchlist
 
-import android.content.Context
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.evangelidis.t_tmoviesseries.R
-import com.evangelidis.t_tmoviesseries.callbacks.OnWatchlistClickCallback
-import com.evangelidis.t_tmoviesseries.databinding.ItemSearchBinding
+import com.evangelidis.t_tmoviesseries.databinding.ItemWatchlistBinding
 import com.evangelidis.t_tmoviesseries.extensions.show
 import com.evangelidis.t_tmoviesseries.room.DatabaseQueries
 import com.evangelidis.t_tmoviesseries.room.WatchlistData
 import com.evangelidis.t_tmoviesseries.utils.Constants.IMAGE_SMALL_BASE_URL
+import com.evangelidis.t_tmoviesseries.utils.InternetStatus
 import com.evangelidis.t_tmoviesseries.utils.ItemsManager.getGlideImage
+import com.evangelidis.tantintoast.TanTinToast
 
-class WatchlistAdapter(
-    var watchlistList: MutableList<WatchlistData>,
-    var callback: OnWatchlistClickCallback,
-    var context: Context
-) : RecyclerView.Adapter<WatchlistAdapter.WatchlistViewHolder>() {
+class WatchlistAdapter(var callback: WatchListItemCallback) : RecyclerView.Adapter<WatchlistAdapter.WatchlistViewHolder>() {
+
+    companion object {
+        const val TV = "TV"
+        const val Movie = "Movie"
+    }
+
+    private val watchList = mutableListOf<WatchlistData>()
 
     fun appendWatchlistData(watchlist: List<WatchlistData>) {
-        watchlistList.clear()
-        watchlistList.addAll(watchlist)
+        watchList.clear()
+        watchList.addAll(watchlist)
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WatchlistViewHolder {
-        return WatchlistViewHolder(ItemSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return WatchlistViewHolder(ItemWatchlistBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    override fun getItemCount(): Int = watchlistList.count()
+    override fun getItemCount(): Int = watchList.count()
 
     override fun onBindViewHolder(holder: WatchlistViewHolder, position: Int) {
-        holder.bind(watchlistList[position], position)
+        holder.bind(watchList[position], position)
     }
 
-    inner class WatchlistViewHolder(private val binding: ItemSearchBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class WatchlistViewHolder(private val binding: ItemWatchlistBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(watchlist: WatchlistData, position: Int) {
-            binding.itemTitle.text = watchlist.name
-            binding.itemReleaseDate.text = watchlist.releasedDate
-            binding.itemType.text = watchlist.category
-            binding.itemRating.text = watchlist.rate.toString()
-            binding.itemWatchlist.apply {
-                setImageResource(R.drawable.ic_enable_watchlist)
-                show()
-            }
-
-            itemView.setOnClickListener { callback.onClick(watchlist) }
-
-            getGlideImage(itemView.context, IMAGE_SMALL_BASE_URL.plus(watchlist.posterPath), binding.itemPoster)
-
-            binding.itemWatchlist.setOnClickListener {
-                DatabaseQueries.removeItem(binding.root.context, watchlist.itemId)
-                remove(position)
+            with(binding) {
+                itemTitle.text = watchlist.name
+                itemReleaseDate.text = watchlist.releasedDate
+                itemType.text = watchlist.category
+                itemRating.text = watchlist.rate.toString()
+                itemWatchlist.apply {
+                    setImageResource(R.drawable.ic_enable_watchlist)
+                    show()
+                    setOnClickListener {
+                        DatabaseQueries.removeItem(root.context, watchlist.itemId) {
+                            remove(position)
+                        }
+                    }
+                }
+                getGlideImage(root.context, IMAGE_SMALL_BASE_URL.plus(watchlist.posterPath), itemPoster)
+                root.setOnClickListener {
+                    if (InternetStatus.getInstance(root.context).isOnline) {
+                        when (watchlist.category) {
+                            TV -> callback.navigateToTvShow(watchlist.itemId)
+                            Movie -> callback.navigateToMovie(watchlist.itemId)
+                        }
+                    } else {
+                        val typeface: Typeface? = ResourcesCompat.getFont(root.context, R.font.montserrat_regular)
+                        TanTinToast.Warning(root.context).text(root.context.getString(R.string.no_internet)).typeface(typeface).show()
+                    }
+                }
             }
         }
 
         private fun remove(position: Int) {
-            watchlistList.removeAt(position)
+            watchList.removeAt(position)
             notifyItemChanged(position)
             notifyItemRangeRemoved(position, 1)
             notifyDataSetChanged()
 
-            if (watchlistList.isEmpty()) {
-                (context as WatchlistActivity).displayEmptyList()
+            if (watchList.isEmpty()) {
+                callback.displayEmptyList()
             }
         }
     }
